65214b2e84f3e634394212073dc7b8831dab39f3
Tamas Soos <tamas.soos@toptal.com>
2022-02-15 20:01:17 +0100

    Changes in webpack.config.js - step 3

--- client-participation/webpack.config.js
+++ client-participation/webpack.config.js
@@ -18,6 +18,25 @@ module.exports = {
   resolve: {
     extensions: [".js", ".css", ".png", ".svg"],
   },
+  plugins: [
+    new LodashModuleReplacementPlugin({
+      "currying": true,
+      "flattening": true,
+      "paths": true,
+      "placeholders": true,
+      "shorthands": true
+    }),
+    new CompressionPlugin({
+      test: /\.js$/,
+      // Leave unmodified without gz ext.
+      // See: https://webpack.js.org/plugins/compression-webpack-plugin/#options
+      filename: '[path][base]',
+      deleteOriginalAssets: true,
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+  ],
   mode: 'production',
   optimization: {
     minimize: true,
