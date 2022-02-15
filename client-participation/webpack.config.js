daf7703ec930cedb6ef18d8fc5d4943dbb6eb803
Tamas Soos <tamas.soos@toptal.com>
2022-02-15 19:29:52 +0100

    Changes in webpack.config.js - step 1

--- client-participation/webpack.config.js
+++ client-participation/webpack.config.js
@@ -1,5 +1,9 @@
+// Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 var path = require("path");
 var webpack = require("webpack");
+var CompressionPlugin = require('compression-webpack-plugin')
+var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
 module.exports = {
   // devtool: "source-map",
@@ -7,9 +11,9 @@ module.exports = {
     "./vis2/vis2"
   ],
   output: {
-    path: path.join(__dirname, "dist_foo"),
+    path: path.join(__dirname, "dist"),
     filename: "vis_bundle.js",
-    publicPath: "SET_THIS_FROM_GULP"
+    publicPath: "/dist/",
   },
   mode: 'production',
   optimization: {
